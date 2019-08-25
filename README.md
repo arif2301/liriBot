b40e49f9af6346a94f414ead4395cbdf67302952
neil <arifrahman@hotmail.com>
2019-08-25 13:19:14 -0400

    assignment completed

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # liriBot
+
+## Purpose 
+This project is called LiriBot. It will take in 3 commands to either search for a movie, a song or a concert by an artist of your choice. it will run on GitBash or on a terminal on visual studio.
+
+## Background on the code
+The application is written in JavaScript. The first are declaring all the variables and required files. Then we have a series of functions to decipher input and provide the necessary output.
+
+
+## Instruction
+To Start, direct your terminal to the liriBot folder. Key in 'npm i', so all necessary JS files are downloaded.
+
+**Once completed**, you are ready to run this program. Key in "node liri.js " followed by one of the 4 commands, a space and then your search query:
+- if you want to search for a concert, type, "concert-this"
+    *example: node liri.js concert-this shawn mendes*
+- if you want to search for a song, type, "spotify-this-song"
+    *example: node liri.js spotify-this-song i feel like summer*
+- if you want to search for a movie, type, "movie-this"
+    *example: node liri.js movie this inception*
+- if you want your instructions from the text file named random.txt, type, "do-what-it-says"
+    *example: node liri.js do-what-it-says*
+    **notes on writing instructions on random.txt:** please key in your instruction as you would in your terminal, ie. concert-this / spotify-this-song /movie-this , followed by a comma and then your search query
+        *example: concert-this, cher*
+
+
+Screen shots included in the screen shots 
+
--- liri.js
+++ liri.js
@@ -1,24 +1,27 @@
-
+// LIRIBOT
 // initiating all the packages
 require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
+var Spotify = require('node-spotify-api');
+var moment = require ("moment");
 
 // this reads the initial command
-var command = 
-process.argv[2];
+var command = process.argv[2];
 var subject = process.argv.slice(3).join("+");
-// slice(3).join + " "
-// var term = process.argv.slice(3).join(" ");
-console.log (command);
-console.log (subject);
+
+// the 3 api calls require search variable in different ways, so they are declared here
+// and not in the function
 var movieName = subject.split("+");
-console.log (movieName);
+var songName = process.argv.slice(3).join(" ");
+var artistName = process.argv.slice(3).join(" ");
 
+//console.log ("movie name " + movieName);
 
+// general welcome banner
 function introduction() {
-    console.log("Welcome to LiriBot");
+    console.log("**************    Welcome to LiriBot   ***************");
 }
     
 // primary function that runs all the commands based on user input
@@ -43,57 +46,102 @@ function runCommand(command) {
 };
 
 function runConcert() {
-
-};
-
-function runMusic() {
-
-};
-
-function runMovie() {
-    axios.get("http://www.omdbapi.com/?t="+subject+"&y=&plot=short&apikey=trilogy").then(
+    console.log ("You have chosen to look for a Concert");
+    // api call for bands in town
+    axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp").then(
     function(response) {
-        console.log("The movie's rating is: " + response.data.imdbRating);
-        console.log("Title of the movie " + response.data.Title);
-        console.log("IMDB Rating of the movie " + response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating of the movie" + response.data.Ratings);
-        console.log("Country where the movie was produced" + response.data.Country);
-        console.log("Language of the movie" + response.data.Language);
-        console.log("Plot of the movie" + response.data.Plot);
-        console.log("Actors in the movie" + response.data.Actors);
-    })
+        console.log("You want to go to a concert for the artist : " + artistName);
+        console.log("Name of the Venue : " + response.data[0].venue.name);
+        console.log("Venue location : " + response.data[0].venue.city + " , " + response.data[0].venue.country);
+        
+        var date = moment(response.data[0].datetime).format("MMM Do YYYY");   
+        console.log("Date of the Event : " + date);
+        //Date of the Event (use moment to format this as "MM/DD/YYYY")
+        })
     .catch(function(error) {
         if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log ("error 1 : The request was made and the server responded with a status code")
         } else if (error.request) {
-        // The request was made but no response was receive
-        console.log(error.request);
+            // The request was made but no response was receive
+            console.log ("error 2 : The request was made but no response was received, Sorry try again")
         } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
+            // Something happened in setting up the request that triggered an Error
+            console.log ("error 3 : Sorry " + artistName + " currently has no upcoming concerts.")
         }
         console.log(error.config);
     });
-   //Title of the movie. - response.data.Title
-   // Year the movie came out.- response.data.Year
-   // IMDB Rating of the movie.- response.data.imdbRating
-   // Rotten Tomatoes Rating of the movie.- response.data.Ratings[{"Rotten Tomatoes"}]
-   // Country where the movie was produced.- response.data.Country
-   // Language of the movie.- response.data.Language
-   // Plot of the movie.- response.data.Plot
-   // Actors in the movie.- response.data.Actors
+};
 
+function runMusic() {
+    // intro and variables
+    console.log ("You have chosen to look for a Song");
+    var spotify = new Spotify(keys.spotify);
+    // actual search query for spotify
+    spotify.search({ type: 'track', query: ("'" + songName + "'" ) })
+    .then(function(response) {
+            console.log ("Name of the Song: " + response.tracks.items[0].name); 
+            console.log ("Name of the Artist: " + response.tracks.items[0].album.artists[0].name); 
+            console.log ("Name of the Album: " + response.tracks.items[0].album.name);
+            console.log ("Link to a sample of the song: ");
+            console.log (response.tracks.items[0].external_urls);
+        
+    })
+    
+    // if search results in an undefined value, we will out put the song 'the sign'
+    .catch(function(err) {
+        spotify.search({ type: 'track', query: ("'the sign Ace of base '" ) })
+        .then(function(response) {
+            console.log ("Sorry Spotify had no results for "+ songName + ". So here another song");
+            console.log ("Name of the Song: " + response.tracks.items[0].name); 
+            console.log ("Name of the Artist: " + response.tracks.items[0].album.artists[0].name); 
+            console.log ("Name of the Album: " + response.tracks.items[0].album.name);
+            console.log ("Link to a sample of the song: ");
+            console.log (response.tracks.items[0].external_urls);
+        })
+    });
+};
+
+function runMovie() {
+    console.log ("You have chosen to look for a movie");
+
+    // if user does not enter a movie, we will select Mr. Nobody for them
+    if (process.argv[3] == null){
+        console.log("You didn't select a movie so we selected one for you");
+        movieName = "Mr Nobody";
+    }
+
+    axios.get("http://www.omdbapi.com/?t="+movieName+"&y=&plot=short&apikey=trilogy").then(
+    function(response) {
+        console.log("Title of the movie : " + response.data.Title);
+        console.log("IMDB Rating of the movie : " + response.data.imdbRating);
+        //i was unsuccessful is making the rotten tomatoes 
+        //console.log("Rotten Tomatoes Rating of the movie :" + response.data.Ratings{2});
+        console.log("Country where the movie was produced : " + response.data.Country);
+        console.log("Language of the movie : " + response.data.Language);
+        console.log("Plot of the movie : " + response.data.Plot);
+        console.log("Actors in the movie : " + response.data.Actors);
+    })
+    .catch(function(error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log ("error 1 : The request was made and the server responded with a status code")
+        } else if (error.request) {
+            // The request was made but no response was receive
+            console.log ("error 2 : The request was made but no response was received, Sorry try again")
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log ("error 3 : Sorry your search query was invalid, try again.")
+        }
+        console.log(error.config);
+    });
 };
 
 function runText() {
-    fs.readFile("best_things_ever.txt", "utf8", function(err, data) {
+    console.log ("You have asked LiriBot to read a text file");
+    fs.readFile("random.txt", "utf8", function(err, data) {
         if (err) {
           return console.log(err);
         }
@@ -101,16 +149,20 @@ function runText() {
         // Breaking  the string down 
         var output = data.split(",");
         command = output [0];
-        subject = output [1];
+        subject = (output[1]).trim();
 
+        // running throught the options again, the global variable used to store user input from the terminal is redefined here to read info from the random.txt file
         switch (command){
             case "concert-this" : 
+                artistName = subject;
                 runConcert();
                 break;
             case "spotify-this-song" : 
+                songName = subject;
                 runMusic();
                 break;
             case "movie-this" : 
+                movieName = subject;
                 runMovie();
                 break;
             default :
@@ -118,7 +170,7 @@ function runText() {
         }       
     });
 }
-
-runCommand(command)
+introduction();
+runCommand(command);
 
 
